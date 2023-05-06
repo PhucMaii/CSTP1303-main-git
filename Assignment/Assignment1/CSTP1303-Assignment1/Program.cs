cc30fd24dcf0f2f0bd522dc5d8133c3249a20e54
Thien Phuc Mai <000461754@student.vcc.ca>
2023-05-06 12:09:20 -0700

    add assignments and labs

---
+++ Assignment/Assignment1/CSTP1303-Assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+            Program program = new Program();
+            var userInput = program.getUserChoice();
+            Console.WriteLine($"You picked option {userInput}");
+        }
+
+        public int getUserChoice()
+        {
+            Console.WriteLine("Pick Option Below");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}. Option {i}");
+            }
+            int option = 0;
+            bool isValid = false;
+
+            while(!isValid)
+            {
+                Console.Write(" ==> ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out option))
+                {
+                    if (option > 0 && option <= 10)
+                    {
+                        isValid = true;
+                    }
+                }
+
+                if(!isValid)
+                {
+                    Console.WriteLine("InValid Input");
+                }
+            }
+            return option;
+        }
+    }
+
+
+}
---
+++ Labs/Lab2/Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _1303Day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.TestReadLine();
+          
+        }
+
+        public void TestReadLine()
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Enter one or more lines - press CTRL+Z to exit):");
+            string line;
+
+            do
+            {
+                Console.Write(" ==> ");
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    Console.WriteLine($" {line}");
+                }
+            } while (line != null);
+        }
+    }
+}
