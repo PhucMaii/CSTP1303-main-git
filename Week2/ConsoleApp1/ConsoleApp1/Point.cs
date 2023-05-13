5916a35c19f12309cc2f9513af41b602240431a3
Thien Phuc Mai <000461754@student.vcc.ca>
2023-05-12 19:48:54 -0700

    add lab3 and week2 folder

---
+++ Week2/ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+    }
+}
---
+++ Week2/Lab3/Lab3/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab3
+{
+    public class User
+    {
+        public int id;
+        public string name;
+    }
+}
