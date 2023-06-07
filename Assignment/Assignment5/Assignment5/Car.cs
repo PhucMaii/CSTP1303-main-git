9ffda3e57dbd4ad02bc834a21729310ba1e39945
Thien Phuc Mai <000461754@student.vcc.ca>
2023-06-07 10:20:40 -0700

    add assignment5

---
+++ Assignment/Assignment5/Assignment5/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Assignment5
+{
+    public class Car<TVehicleType, TEngine, TBody> : Vehicle<TVehicleType, TEngine, TBody>
+    {
+        public Car(TVehicleType t, TEngine e, TBody b) : base(t, e, b)
+        {
+        }
+
+
+    }
+}
+
---
+++ Assignment/Assignment5/Assignment5/Vehicle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public class Vehicle<TVehicleType, TEngine, TBody> 
+    {
+        protected TVehicleType type;
+        private TEngine engine;
+        private TBody body;
+
+        public Vehicle(TVehicleType t, TEngine e, TBody b) 
+        {
+            this.type = t;  
+            this.engine = e;
+            this.body = b;
+        }
+
+        public void SetEngine(TEngine e)
+        {
+            this.engine = e;
+        }
+
+        public TEngine GetEngine() 
+        {
+            return this.engine;
+        }
+
+        public void SetType(TVehicleType t)
+        {
+            this.type = t;
+        }
+
+        public TVehicleType GetVehicleType()
+        {
+            return this.type;
+        }
+
+        public void SetBody(TBody b)
+        {
+            this.body = b;
+        }
+
+        public TBody GetBody()
+        {
+            return this.body;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Vehicle Type: {this.GetVehicleType()}, Body: {this.GetBody()}, Engine: {this.GetEngine()}");
+        }
+    }
+}
