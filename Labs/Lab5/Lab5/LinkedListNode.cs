0a054172c22e65f12cab156b1064567404f45300
Thien Phuc Mai <000461754@student.vcc.ca>
2023-06-24 12:04:42 -0700

    add lab5

---
+++ Labs/Lab5/Lab5/LinkedListNode.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab5
+{
+    public class LinkedListNode<T>
+    {
+        public T Data { get; set; }
+
+        public LinkedListNode(T data)
+        {
+            this.Data = data;
+        }
+
+        public LinkedListNode<T> Next { get; set; }
+    }
+}
---
+++ Labs/Lab5/Lab5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddTail(new LinkedListNode<int>(85));
+            list.AddTail(new LinkedListNode<int>(19));
+            list.AddTail(new LinkedListNode<int>(20));
+            list.AddTail(new LinkedListNode<int>(1));
+            list.AddTail(new LinkedListNode<int>(38));
+
+
+
+
+            // List before reversal
+            Console.WriteLine("Given linked list ");
+            list.PrintList();
+
+            // Reverse the list
+            list.ReverseLinkedList();
+
+            // List after reversal
+            Console.WriteLine("Reversed linked list ");
+            list.PrintList();
+        }
+    }
+}
