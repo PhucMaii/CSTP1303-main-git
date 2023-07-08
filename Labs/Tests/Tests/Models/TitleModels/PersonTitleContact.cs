c501cd5c744dff3000b3b2ff015a41abe95d51ca
Thien Phuc Mai <000461754@student.vcc.ca>
2023-07-08 12:53:57 -0700

    add TestProject

---
+++ Labs/Tests/Tests/Models/TitleModels/PersonTitleContact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Models.TitleModels
+{
+    public class PersonTitleContact : Contact
+    {
+        private _title Title;
+
+        public PersonTitleContact(string email, string phoneNumber, _title title) : base(email, phoneNumber)
+        {
+            Title = title;
+        }
+    }
+}
