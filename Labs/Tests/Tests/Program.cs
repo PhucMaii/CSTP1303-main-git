06a28b0ea3d18615ae923e7bd264aa979e1f94e7
PhucMaii <maithienphuc0102@gmail.com>
2023-07-08 18:00:03 -0700

    add part1 of project

--- Labs/Tests/Tests/Program.cs
+++ Labs/Tests/Tests/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Tests 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ContactManager contactManager = new ContactManager();
+        }
+    }
+
+}
