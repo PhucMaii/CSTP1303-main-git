26b7b8b840ae235d055c5c2cba317e985957d88a
PhucMaii <maithienphuc0102@gmail.com>
2023-07-14 22:17:31 -0700

    update classes

---
+++ Labs/Tests/Tests/Models/TitleModels/DepartmentContact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Models.TitleModels
+{
+    public class DepartmentContact : Contact
+    {
+        private _title Department;
+
+        public DepartmentContact(string email, string phone, _title department) : base(email, phone) 
+        {
+            Department = department;
+        }
+
+    }
+}
