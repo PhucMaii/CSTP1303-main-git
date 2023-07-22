a29014ed386449c0d104117228267907ac4fad7b
Thien Phuc Mai <000461754@student.vcc.ca>
2023-07-22 10:22:47 -0700

    add hierachy structure

---
+++ Labs/Tests/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactStore
+{
+    public class Entity
+    {
+        private int Id { get; }
+        private string Name { get; set; }
+    }
+}
