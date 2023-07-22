9dd8971fadd1f5145bc6c8f866e3427c06a5b340
Thien Phuc Mai <000461754@student.vcc.ca>
2023-07-22 10:01:39 -0700

    change structure

---
+++ Labs/Tests/Abstraction/ICompany.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public interface ICompany
+    {
+        public string Id { get; }
+        public string Name { get; set; }
+    }
+}
---
+++ Labs/Tests/Models/Company.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+namespace Abstraction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class Company : ICompany
+    {
+        public string Id { get;  }
+        public string Name { get; set; }
+
+        public Company(string id, string name)
+        {
+            if(string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException("Company id is required");
+            }
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("Company name is required");
+            }
+            Id = id;
+            Name = name;
+        }
+
+    }
+}
